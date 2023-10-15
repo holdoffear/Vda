ba83b83b4559a8da1b300ca42e1765472060a068
holdoffear <holdoffear@protonmail.com>
2023-10-15 18:52:12 +0200

    Updated tests

--- tests/Vda.Tests/DynamicArrayTest.cs
+++ tests/Vda.Tests/DynamicArrayTest.cs
@@ -1,3 +1,5 @@
+using System.IO.IsolatedStorage;
+
 namespace Vda.Tests
 {
     [TestClass]
@@ -79,6 +81,100 @@ namespace Vda.Tests
                 bool result = dynamicArray.Contains(employees[i]);
                 Assert.IsTrue(result);
             }
+        }
+        [TestMethod]
+        [DataRow(new int[]{31, 43, 4, 1, 0, 90, 11}, 3)]
+        [DataRow(new int[]{31, 43, 4, 1, 0, 90, 11}, 0)]
+        [DataRow(new int[]{31, 43, 4, 1, 0, 90, 11}, 7)]
+        public void CreateSpan_Array_ReturnsArray(int[] array, int lastIndex)
+        {
+            Span<int> newSpan = DynamicArray<int>.CreateSpan(array, 0, lastIndex);
+
+            int result = newSpan.Length;
+
+            Assert.AreEqual(lastIndex, result);
+
+            for (int i = 0; i < newSpan.Length; i++)
+            {
+                Assert.AreEqual(array[i], newSpan[i]);
+            }
+        }
+        [TestMethod]
+        [DataRow(new int[]{1, 5, 3, 8, 11}, 0, typeof(int))]
+        [DataRow(new int[]{3, 2, 5, 8, 6, 4, 0}, 3, typeof(int))]
+        [DataRow(new int[]{0, 3, 0, 1, 4, 7}, 5, typeof(int))]
+        public void Resize_IntArray_ReturnsTrue(int[] inputArray, int newSize, Type inputType)
+        {
+            Array newArray = Array.CreateInstance(inputType, inputArray.Length);
+            Array.Copy(inputArray, newArray, inputArray.Length);
+
+            DynamicArray<int>.Resize(ref inputArray, newSize);
+            Assert.AreEqual(newSize, inputArray.Length);
+            
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int result = inputArray[i];
+                Assert.AreEqual(newArray.GetValue(i), result);
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(EmployeeData))]
+        public void Remove_MultipleStructs_ReturnsFalse(object[] inputData)
+        {
+            Employee[] employees = CreateEmployees(inputData);
+            if (employees.Length <= 0)
+            {
+                return;
+            }
+            Employee removeEmployee = employees[0];
+            DynamicArray<Employee> dynamicArray = new DynamicArray<Employee>();
+            for (int i = 0; i < employees.Length; i++)
+            {
+                dynamicArray.Add(employees[i]);
+            }
+            dynamicArray.Remove(removeEmployee);
+
+            int size = dynamicArray.Length;
+            bool result = dynamicArray.Contains(removeEmployee);
+
+            Assert.AreEqual(employees.Length-1, size);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        [DynamicData(nameof(EmployeeData))]
+        public void RemoveAt_MultipleStructs_ReturnsFalse(object[] inputData)
+        {
+            Employee[] employees = CreateEmployees(inputData);
+            if (employees.Length <= 0)
+            {
+                return;
+            }
+            Employee removeEmployee = employees[0];
+            DynamicArray<Employee> dynamicArray = new DynamicArray<Employee>();
+            for (int i = 0; i < employees.Length; i++)
+            {
+                dynamicArray.Add(employees[i]);
+            }
+            dynamicArray.RemoveAt(0);
+
+            int size = dynamicArray.Length;
+            bool result = dynamicArray.Contains(removeEmployee);
+
+            Assert.AreEqual(employees.Length-1, size);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        [DataRow(new int[]{1, 5, 3, 8, 11}, 0, 4)]
+        [DataRow(new int[]{3, 2, 5, 8, 6, 4, 0}, 2, 3)]
+        [DataRow(new int[]{0, 3, 0, 1, 4, 7}, 1, 4)]
+        public void SwapIndices_IntArray_ReturnsTrue(int[] inputArray, int indexA, int indexB)
+        {
+            int resultA = inputArray[indexA];
+            int resultB = inputArray[indexB];
+            DynamicArray<int>.SwapIndices(inputArray, indexA, indexB);
+
+            Assert.AreEqual(resultA, inputArray[indexB]);
+            Assert.AreEqual(resultB, inputArray[indexA]);
         }
     }
 
